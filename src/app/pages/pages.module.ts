b6004d498e6484755c78823a89fd865841174409
imanol309 <imanolguzman309@gmail.com>
2022-02-05 23:57:50 -0400

    limpiando el proyecto

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavBarComponent } from './home/components/nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
-    NavBarComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule
   ],
