6f9dbee655ff97485964a2ef3f26a42edd813948
imanol309 <imanolguzman309@gmail.com>
2022-02-06 22:43:57 -0400

    Agregando texto al navbar

--- src/app/pages/home.module.ts
+++ src/app/pages/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './home/components/nav-bar/nav-bar.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, NavBarComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
