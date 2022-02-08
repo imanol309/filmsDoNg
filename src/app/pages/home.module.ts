43c57a692bc33681a47f3bc54de708b28c50ab86
imanol309 <imanolguzman309@gmail.com>
2022-02-08 13:26:57 -0400

    Agregando un nuevo modulo carousel

--- src/app/pages/home.module.ts
+++ src/app/pages/home.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './home/components/nav-bar/nav-bar.component';
+import { CarouselComponent } from './home/components/carousel/carousel.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, NavBarComponent],
+  declarations: [HomeComponent, NavBarComponent, CarouselComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
