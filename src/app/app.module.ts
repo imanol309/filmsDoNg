9c26f769eff3f1a39b907ff85d03f0aaa0088464
imanol309 <imanolguzman309@gmail.com>
2022-02-18 16:01:49 -0400

    Solucionando error del servidor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { appRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
 import { HomeModule } from './pages/home.module';
 import { HomeComponent } from './pages/home/home.component';
 @NgModule({
@@ -11,6 +12,7 @@ import { HomeComponent } from './pages/home/home.component';
   imports: [
     BrowserModule,
     appRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
