fcc53fd3b821b3fd71c9049bcff7a4a79c93a2c9
imanol309 <imanolguzman309@gmail.com>
2022-04-01 12:00:06 -0400

    Agregando el unsubscribe a mi proyecto, movie-section

--- src/app/pages/home/components/movie-sections/movie-sections.component.ts
+++ src/app/pages/home/components/movie-sections/movie-sections.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HomeService } from '../../services/home.service';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-movie-sections',
   templateUrl: './movie-sections.component.html',
   styleUrls: ['./movie-sections.component.css'],
 })
-export class MovieSectionsComponent implements OnInit {
+export class MovieSectionsComponent implements OnInit, OnDestroy {
   datos = null;
+  subs = new SubSink();
   constructor(private homeServices: HomeService) {}
+
   ngOnInit(): void {
     this.verDatos();
   }
 
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
+
   verDatos() {
-    this.homeServices
-      .getPelis()
-      .subscribe((arg) => {
-        this.datos = arg
-      })
+    this.subs.sink = this.homeServices.getPelis().subscribe((arg) => {
+      this.datos = arg;
+    });
   }
 }
