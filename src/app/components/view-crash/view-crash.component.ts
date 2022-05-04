c98d2307e924415737630ae20ae323f82476062b
imanol309 <imanolguzman309@gmail.com>
2022-05-04 17:14:25 -0400

    Agregando funcion para cerrar los mat-dialog

--- src/app/components/view-crash/view-crash.component.ts
+++ src/app/components/view-crash/view-crash.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { HomeDateMovie } from 'src/app/pages/home/models/home.model';
+import { ModalSeekerComponent } from '../modal-seeker/modal-seeker.component';
 
 @Component({
   selector: 'app-view-crash',
@@ -10,12 +12,15 @@ import { HomeDateMovie } from 'src/app/pages/home/models/home.model';
 export class ViewCrashComponent implements OnInit {
   @Input() datosPelicula: HomeDateMovie;
   @Input() Css: boolean;
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    // public dialogRef: MatDialogRef
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   routerMovies(titulo: String) {
-    this.router.navigate(['/pelis/movies/'+titulo]);
+    // this.dialogRef.close();
+    this.router.navigate(['/pelis/movies/' + titulo]);
   }
 }
