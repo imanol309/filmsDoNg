00ef440f0075b7b336debe86409d0af1fc41fee2
imanol309 <imanolguzman309@gmail.com>
2022-05-11 16:57:43 -0400

    Solucionando error de no poder vista las videos

--- src/app/components/view-crash/view-crash.component.ts
+++ src/app/components/view-crash/view-crash.component.ts
@@ -13,17 +13,12 @@ export class ViewCrashComponent implements OnInit, OnDestroy {
   @Input() loanding: boolean;
   numSlice: any = 15;
   cssVer: boolean = true;
-  screenWidth = screen.width;
-  varWidthImg: string;
-  varWidthContainer: string;
   constructor(private router: Router) {}
 
   ngOnInit(): void {
     if (!this.Css) {
       this.cssVer = false;
     }
-    console.log(this.screenWidth);
-    console.log(this.functionWidth());
   }
 
   ngOnDestroy(): void {
@@ -40,23 +35,6 @@ export class ViewCrashComponent implements OnInit, OnDestroy {
       if (this.numSlice > this.datosPelicula.length) {
         this.cssVer = false;
       }
-    }
-  }
-
-  functionWidth() {
-    if (this.screenWidth <= 350) {
-      this.varWidthImg = 'carhImg carh350'
-      this.varWidthContainer = 'containerCarh conatainer350'
-    }
-
-    if (this.screenWidth <= 500) {
-      this.varWidthImg ='carhImg carh500';
-      this.varWidthContainer = 'containerCarh conatainer500'
-    }
-
-    if (this.screenWidth <= 650) {
-      this.varWidthImg ='carhImg carh650';
-      this.varWidthContainer = 'containerCarh conatainer650'
     }
   }
 }
--- src/app/pages/viewing-movies/viewing-movies.component.ts
+++ src/app/pages/viewing-movies/viewing-movies.component.ts
@@ -2,26 +2,30 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SubSink } from 'subsink';
 import { ViewingMoviesService } from './services/viewing-movies.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-viewing-movies',
   templateUrl: './viewing-movies.component.html',
-  styleUrls: ['./viewing-movies.component.css']
+  styleUrls: ['./viewing-movies.component.css'],
 })
 export class ViewingMoviesComponent implements OnInit {
   peliDato: any;
   subs = new SubSink();
-  loanding:  boolean = false
-  constructor(private rutaActiva: ActivatedRoute,
-    private viewingMoviesService: ViewingMoviesService) { 
-      this.rutaActiva.paramMap.subscribe(params => {
-        this.peliDato = params.get('idpeli')
-      })
-      this.datosUnicos(this.peliDato)
-    }
+  loanding: boolean = false;
+  constructor(
+    private rutaActiva: ActivatedRoute,
+    private viewingMoviesService: ViewingMoviesService,
+    private sanitizer: DomSanitizer
+  ) {
+    this.rutaActiva.paramMap.subscribe((params) => {
+      this.peliDato = params.get('idpeli');
+    });
+    this.datosUnicos(this.peliDato);
+  }
 
   ngOnInit(): void {
-    this.loanding = true
+    this.loanding = true;
   }
 
   ngOnDestroy(): void {
@@ -29,11 +33,16 @@ export class ViewingMoviesComponent implements OnInit {
   }
 
   datosUnicos(value: any): any {
-    this.subs.sink = this.viewingMoviesService.getPelisTitulo(value).subscribe((datos) => {
-      console.log(datos)
-      this.peliDato = datos;
-      this.loanding = false
-    });
+    this.subs.sink = this.viewingMoviesService
+      .getPelisTitulo(value)
+      .subscribe((datos) => {
+        console.log(datos);
+        this.peliDato = datos;
+        this.loanding = false;
+      });
   }
-  
+
+  transform(url) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
