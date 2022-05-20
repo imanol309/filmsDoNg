438d8e91e383b6ca6f38bd087f4e2266fa4638fb
imanol309 <imanolguzman309@gmail.com>
2022-05-20 10:10:18 -0400

    Modificando la pagina de mi lista

--- src/app/pages/list-movies/list-movies.component.ts
+++ src/app/pages/list-movies/list-movies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GenericService } from 'src/app/services/generic.service';
 
 @Component({
   selector: 'app-list-movies',
@@ -9,13 +10,21 @@ export class ListMoviesComponent implements OnInit {
   datoUsuario: any;
   css: boolean = true
   loanding: boolean = false
-  constructor() { }
+  constructor(private genericService: GenericService) { }
 
   ngOnInit() {
     this.loanding = true
-    this.datoUsuario = JSON.parse(localStorage.getItem('usuario'))
-    this.datoUsuario = this.datoUsuario?.favoriteMovies
-    this.loanding = false
+    const datosFormulario = JSON.parse(localStorage.getItem('Formulario'));
+    this.datosLista(datosFormulario)
   }
+  
 
+  datosLista(datos) {
+    this.genericService.postLogin(datos).subscribe((datos) => {
+      localStorage.setItem('usuario', JSON.stringify(datos?.DatosLogin));
+      this.datoUsuario = JSON.parse(localStorage.getItem('usuario'));
+      this.datoUsuario = this.datoUsuario?.favoriteMovies
+      this.loanding = false
+    })
+  }
 }
