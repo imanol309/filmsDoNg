89cf9e5cd590e8e8517e23045972843bfaf89450
imanol309 <imanolguzman309@gmail.com>
2022-06-08 16:06:39 -0400

    Solucionando vista de android

--- src/app/components/view-crash/view-crash.component.ts
+++ src/app/components/view-crash/view-crash.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, Input, OnDestroy, ViewChild, Renderer2, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnDestroy,
+  ViewChild,
+  Renderer2,
+  ElementRef,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { HomeDateMovie } from 'src/app/pages/home/models/home.model';
 import { GenericService } from 'src/app/services/generic.service';
@@ -20,28 +28,28 @@ export class ViewCrashComponent implements OnInit, OnDestroy {
   numSlice: any = 15;
   cssVer: boolean = true;
   arrayId: any = [];
-  
+
   constructor(private router: Router, private genericService: GenericService) {}
-  
+
   ngOnInit(): void {
     if (!this.Css) {
       this.cssVer = false;
     }
-    this.datoUsuario = JSON.parse(localStorage.getItem('usuario'))
-    this.forGuardad()
+    this.datoUsuario = JSON.parse(localStorage.getItem('usuario'));
+    this.forGuardad();
   }
 
-  imgGuardad(d, datos){
-    this.agregarPelicula(datos)
-    d.src = "https://films-do-ng.vercel.app/assets/img/icon/guardar-instagram-guardado.png"
-   }
+  imgGuardad(d, datos) {
+    this.agregarPelicula(datos);
+    d.src =
+      'https://films-do-ng.vercel.app/assets/img/icon/guardar-instagram-guardado.png';
+  }
 
-   
-  imgEliminar(d, id){
-    this.eliminarPelicula(id)
-    d.src = "https://films-do-ng.vercel.app/assets/img/icon/guardar-instagram-sin-guarda.png"
-   }
-
+  imgEliminar(d, id) {
+    this.eliminarPelicula(id);
+    d.src =
+      'https://films-do-ng.vercel.app/assets/img/icon/guardar-instagram-sin-guarda.png';
+  }
 
   ngOnDestroy(): void {
     this.numSlice = 15;
@@ -58,49 +66,48 @@ export class ViewCrashComponent implements OnInit, OnDestroy {
 
   verCargarMas() {
     if (this.numSlice <= this.datosPelicula?.length) {
-      return true
+      return true;
     } else {
-      return false
+      return false;
     }
   }
 
   agregarPelicula(datos) {
     this.genericService.postAddMovieList(datos).subscribe((datoGuradado) => {
       localStorage.setItem('usuario', JSON.stringify(datoGuradado?.moviesNew));
-      this.datoUsuario = JSON.parse(localStorage.getItem('usuario'))
-      this.forGuardad()
-    })
+      this.datoUsuario = JSON.parse(localStorage.getItem('usuario'));
+      this.forGuardad();
+    });
   }
 
   eliminarPelicula(id) {
     this.genericService.putDeleteMyList(id).subscribe((datos) => {
       localStorage.setItem('usuario', JSON.stringify(datos?.moviesNew));
-      this.datoUsuario = JSON.parse(localStorage.getItem('usuario'))
-      this.forGuardad()
-    })
+      this.datoUsuario = JSON.parse(localStorage.getItem('usuario'));
+      this.forGuardad();
+    });
   }
-    
+
   forGuardad() {
-    this.arrayId = []
+    this.arrayId = [];
     this.datoUsuario.favoriteMovies?.map((x) => {
-      this.arrayId.push(x._id)
-    })
+      this.arrayId.push(x._id);
+    });
   }
 
   ifGuardad(d) {
     if (this.arrayId.includes(d)) {
-      return '../../../../../assets/img/icon/guardar-instagram-guardado.png'
+      return '../../../../../assets/img/icon/guardar-instagram-guardado.png';
     } else {
-      return '../../../../../assets/img/icon/guardar-instagram-sin-guarda.png'
+      return '../../../../../assets/img/icon/guardar-instagram-sin-guarda.png';
     }
   }
 
   ifClases(d) {
     if (this.arrayId.includes(d)) {
-      return 'imgGuardad'
+      return 'imgGuardad';
     } else {
-      return 'imgGuardad'
+      return 'imgGuardad';
     }
   }
-
 }
