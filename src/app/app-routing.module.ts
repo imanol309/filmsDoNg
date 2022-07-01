a05f80ad317ac9a79fb65c0f6409104db6f59759
imanol309 <imanolguzman309@gmail.com>
2022-07-01 16:53:43 -0400

    Agregando campo de busqueda no encontrada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,26 @@
-import { NgModule } from "@angular/core";
-import {Routes, RouterModule} from "@angular/router"
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-    {
-        path: 'pelis',
-        loadChildren: () => import('./pages/pages.module').then((m) => m.pagesModule)
-    },
-    {
-        path: 'error404',
-        loadChildren: () => import('./pages/error404/error404.module').then((m) => m.Error404Module)
-    },
-    {
-        path: '**', redirectTo: 'pelis'
-    }
+  {
+    path: 'pelis',
+    loadChildren: () =>
+      import('./pages/pages.module').then((m) => m.pagesModule),
+  },
+  {
+    path: 'error404',
+    loadChildren: () =>
+      import('./pages/error404/error404.module').then((m) => m.Error404Module),
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'pelis',
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-
 export class appRoutingModule {}
