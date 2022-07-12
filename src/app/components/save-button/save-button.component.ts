b12222900344fabf124852d58ac3d4346357fd48
imanol309 <imanolguzman309@gmail.com>
2022-07-12 10:47:17 -0400

    Guardar boton de guardar

---
+++ src/app/components/save-button/save-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-save-button',
+  templateUrl: './save-button.component.html',
+  styleUrls: ['./save-button.component.css']
+})
+export class SaveButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
